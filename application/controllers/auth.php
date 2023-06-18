4b7424856a45553dcc89e734e7640f640e19dc60
Razif Ilham Rosadi <razifilham.12@gmail.com>
2023-06-19 06:45:26 +0700

    ini baru sampai coding di user registration menit 25 WPU CI3

---
+++ application/controllers/auth.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$this->load->view('templates/auth_header');
+		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
+	}
+
+	public function registration()
+	{
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[6]|matches[password2]', [
+			'matches' => 'Password does not match!',
+			'min_length' => 'Password too short!'
+		]);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = 'Registration';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		} else {
+			echo 'Data berhasil ditambahkan!';
+		}
+	}
+}
---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,11 @@
+<!--   Core JS Files   -->
+<script src="<?= base_url('assets/'); ?>js/core/popper.min.js" type="text/javascript"></script>
+<script src="<?= base_url('assets/'); ?>js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?= base_url('assets/'); ?>js/plugins/perfect-scrollbar.min.js"></script>
+<!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+<!--  Google Maps Plugin    -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+<script src="<?= base_url('assets/'); ?>js/material-kit.min.js?v=3.0.4" type="text/javascript"></script>
+</body>
+
+</html>
